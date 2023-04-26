1d83471fd852274b878c3c3b0e7b2d92d053a13f
Rodrigo <rodrigo.garra77@hotmail.com>
2023-04-25 21:19:09 -0300

    exercicio 1.0

--- js/main.js
+++ js/main.js
@@ -4,3 +4,45 @@ $(document).ready(function(){
     $("#cpf").mask('000.000.000-00');
     $("#cep").mask('00000‑000 ');
 })
+
+
+$('form').validate({
+    rules: {
+        nome:  {
+            required: true,
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true,
+        },
+        cpf: {
+            required: false,
+        },
+        endereco: {
+            required: true,
+        },
+        cep: {
+            required: true,
+        }
+    },
+    messages: {
+        nome: 'Por favor, insire seu nome!',
+        email: 'Por favor, insire seu email!',
+        telefone: 'Por favor, insire seu telefone!',
+        endereco: 'Por favor, insire seu endereco!',
+        cep: 'Por favor, insire seu cep!'
+    },
+    submitHandler: function(form) {
+        console.log(form)
+    },
+    invalidHandler: function(evento, validador){
+        let camposIncorretos = validador.numberOfInvalids();
+        if (camposIncorretos){
+            alert(`Existem ${camposIncorretos} campos incorretos.`)
+        }
+    } 
+
+})
