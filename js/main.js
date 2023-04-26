2a98baf45957fbb89fc64cd8ebddb56d575cdc54
Rodrigo <rodrigo.garra77@hotmail.com>
2023-04-25 21:16:33 -0300

    exercicio

--- js/main.js
+++ js/main.js
@@ -1,38 +1,6 @@
-$("#telefone").mask('(00) 00000-0000')
+$(document).ready(function(){
 
-    $('form').validate({
-        rules: {
-            nome:  {
-                required: true,
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            telefone: {
-                required: true,
-            },
-            carroDeInteresse: {
-                required: false,
-            },
-            obs: {
-                required: true,
-            }
-        },
-        messages: {
-            nome: 'Por favor, insire seu nome!',
-            email: 'Por favor, insire seu email!',
-            telefone: 'Por favor, insire seu telefone!',
-            obs: 'Por favor, insire sua observacao!'
-        },
-        submitHandler: function(form) {
-            console.log(form)
-        },
-        invalidHandler: function(evento, validador){
-            let camposIncorretos = validador.numberOfInvalids();
-            if (camposIncorretos){
-                alert(`Existem ${camposIncorretos} campos incorretos.`)
-            }
-        }
-    
-    })
+    $("#telefone").mask('(00) 00000-0000');
+    $("#cpf").mask('000.000.000-00');
+    $("#cep").mask('00000‑000 ');
+})
